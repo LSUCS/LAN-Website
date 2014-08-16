574e11491f34777c71a7b9f2d9bd92d3ef969365
MetalMichael <mfiford@lsucs.org.uk>
2014-08-16 17:34:16 +0100

    config.php should be part of repository

---
+++ config.php
@@ -0,0 +1,38 @@
+<?php
+        
+    include "/home/soc_lsucs/lan.lsucs.org.uk/password.php";
+    
+    /**
+     * Base LanWebsite Library Directory
+     */
+    $config['libdir'] = "/home/soc_lsucs/lan.lsucs.org.uk/htdocs/lib/";
+    
+    /**
+     * Database Settings
+     */
+    $config['database']["host"] = "localhost";
+    $config['database']["user"] = "lanwebsite";
+    $config['database']["pass"] = $password;
+    $config['database']["db"]   = "lanwebsite";
+    
+    /**
+     * Default Controller location
+     */
+    $config['controllerdir'] = '/public/controllers/';
+    
+    /**
+     * Auth Mechanism
+     */
+    $config['auth'] = "LanWebsite_Auth_Lsucs";
+
+    /**
+     * UserManager
+     */
+    $config['usermanager'] = "LanWebsite_UserManager_Lsucs";
+    
+    /**
+     * SEO-friendly URLs
+     */
+    $config['seo_enabled'] = true;
+
+?>
